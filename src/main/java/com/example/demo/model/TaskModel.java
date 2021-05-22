1469d0429f15007e3991fcf54821a73021fb7d2d
Takafumi <takafumi.a304@gmail.com>
2021-05-22 19:00:23 +0900

    新規タスク登録用のモデルクラスを追加

---
+++ src/main/java/com/example/demo/model/TaskModel.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+public class TaskModel {
+	
+	private String contents;
+	private boolean complete;
+	
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public boolean isComplete() {
+		return complete;
+	}
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+}
